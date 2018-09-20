c09fa86446ecb16c2acd81fc4febc83ed93641b4
Edward Barak <edward.barak@gmail.com>
2018-09-20 00:07:45 -0400

    added good scanning formula to comment

--- bwScan.py
+++ bwScan.py
@@ -1,4 +1,5 @@
 from PIL import Image
+from subprocess import call
 import os
 import glob
 
@@ -34,4 +35,23 @@ def rename_reduce_nums(path, filetype, start, stop, step=1, unary=1 ):
     for fnum in fnums:
         os.rename(path + str(fnum) + '.' + filetype, path + str(fnum + unary) + '.' + filetype)
 
+def exif_stripAll(path, filetype):
+   # for root, dirs, files in os.walk(path):
+   #     for f in files:
+   #         if f[-1 * len(filetype):].lower() == filetype.lower():
+   #             call(['mogrify', '-strip', root + f])
+   #             print('Stripped ' + root + f)
+
+    pass
+
+def optimize(path, sname, im, scale_factor=1, quality=75):
+    opt = im.convert('P', palette=Image.ADAPTIVE, colors=5)
+    opt = opt.resize((opt.width * scale_factor, opt.height * scale_factor), Image.ANTIALIAS)
+    opt = opt.convert('L')
+    opt.save(path + sname, optimize=True, quality=quality)
+
+
 pwd = os.getcwd()
+
+# another good formula
+# Image.open(images[randint(0,len(images))]).convert('L').filter(ImageFilter.Kernel((3,3), [4, 3, 2,1,0,1,1,2,2], scale=1, offset=-1024*2.62)).convert('1', dither=False).show()
